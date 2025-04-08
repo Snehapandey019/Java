2277875aff605d6f66afd12aef2cf980e132fd40
Sneha Pandey <sneha.cs231210@global.org.in>
2025-04-08 11:15:20 +0530

    datatypes

---
+++ datatypes.java
@@ -0,0 +1,22 @@
+class datatypes{
+    public static void main(String[] args) {
+        byte a =34;
+        System.out.println(a);
+
+        short b =45;
+        System.out.println(b);
+
+        int c =123;
+        System.out.println(c);
+
+        float d = 12.34f;
+        System.out.println(d);
+
+        double e = 456789.1235456;
+        System.out.println(e);
+
+        char f = 'A';
+        System.out.println(f);
+        
+ }
+}
