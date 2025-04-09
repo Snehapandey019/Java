284656a6d1ba609c179af0c81727967bb5fe58c4
Sneha Pandey <sneha.cs231210@global.org.in>
2025-04-09 21:12:56 +0530

    hello

---
+++ ternary.java
@@ -0,0 +1,10 @@
+class ternary {
+    public static void main(String[] args){
+         int a = 10 ;
+         int b = 20 ;
+         System.out.println(a<b?"hii":"hello");
+         int c = 40;
+         int d = 30;
+        System.out.println(c<d?"hii":"hello");
+            }
+}
