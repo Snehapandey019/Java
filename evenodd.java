fd85061823c593c3b70f350d2b449195cff49f11
Sneha Pandey <sneha.cs231210@global.org.in>
2025-04-10 20:37:06 +0530

    hello

---
+++ evenodd.java
@@ -0,0 +1,17 @@
+//Check whether a number is even or odd
+class evenodd{
+    public static void main(String[] args) {
+        int n = 10;
+        if (n % 2 == 0) {
+            System.out.println(n + " is even");
+        } else {
+            System.out.println(n + " is odd");
+        }
+        int y = 5;
+        if (y % 2 == 0) {
+            System.out.println(y + " is even");
+        } else {
+            System.out.println(y + " is odd");
+        }
+    }
+}
