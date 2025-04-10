b0e0250d2ea9a0faf4e38f5610dd9697b5ce9513
Sneha Pandey <sneha.cs231210@global.org.in>
2025-04-10 20:41:45 +0530

    hello

---
+++ positivenegativezero.java
@@ -0,0 +1,29 @@
+//Check if a number is positive, negative, or zero
+class positivenegativezero {
+    public static void main(String[] args) {
+        int n = 10;
+        if (n > 0) {
+            System.out.println(n + " is positive");
+        } else if (n < 0) {
+            System.out.println(n + " is negative");
+        } else {
+            System.out.println(n + " is zero");
+        }
+        int y = -5;
+        if (y > 0) {
+            System.out.println(y + " is positive");
+        } else if (y < 0) {
+            System.out.println(y + " is negative");
+        } else {
+            System.out.println(y + " is zero");
+        }
+        int z = 0;
+        if (z > 0) {
+            System.out.println(z + " is positive");
+        } else if (z < 0) {
+            System.out.println(z + " is negative");
+        } else {
+            System.out.println(z + " is zero");
+        }
+    }
+}
