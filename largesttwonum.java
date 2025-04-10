3e342ca922ecfe817c8e8ab34fb22b2bc2c5545f
Sneha Pandey <sneha.cs231210@global.org.in>
2025-04-10 20:47:26 +0530

    hello

---
+++ largesttwonum.java
@@ -0,0 +1,19 @@
+//Find the largest of two numbers
+class largesttwonum{
+    public static void main(String args[]){
+       {
+         int a = 17, b = 25;
+        if(a > b)
+            System.out.println("Largest number is: " + a);
+        else
+            System.out.println("Largest number is: " + b);
+       }
+           {
+            int c = 24, d =19 ;
+        if(c > d)
+            System.out.println("Largest number is: " + c);
+        else
+            System.out.println("Largest number is: " + d);
+           }
+    }
+}
