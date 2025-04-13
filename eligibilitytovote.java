a466b7f483de7f60f9bc2a9a10ad7cbbce2d1b0c
Sneha Pandey <sneha.cs231210@global.org.in>
2025-04-13 20:32:12 +0530

    hello

---
+++ eligibilitytovote.java
@@ -0,0 +1,10 @@
+// Check eligibility to vote
+class eligibilitytovote {
+public static void main(String[] args) {
+    int age = 20;
+if (age >= 18)
+ System.out.println("Eligible to vote");
+else
+ System.out.println("Not eligible");
+}
+}
