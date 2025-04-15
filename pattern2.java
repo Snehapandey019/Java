932de3ec4a5033de8e0ab9ae482b8912994b34f1
Sneha Pandey <sneha.cs231210@global.org.in>
2025-04-15 22:13:49 +0530

    hello

--- pattern2.java
+++ pattern2.java
@@ -2,7 +2,7 @@ class pattern2 {
     public static void main(String[] args){
         int n = 5;
         for (int i = 1; i <= n; i++){
-            for (int j = 5; j > = i ; j--){
+            for (int j = 5; j >= i ; j--){
                 System.out.print("* ");
             }
             System.out.println();
