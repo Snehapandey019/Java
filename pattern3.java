31338d2c9e6a18ed0971ebe347d90e167ab167b2
Sneha Pandey <sneha.cs231210@global.org.in>
2025-04-15 13:49:47 +0530

    hello

---
+++ pattern3.java
@@ -0,0 +1,18 @@
+class pattern3{
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+            for ( int j=5; j>=i;j--){
+                System.out.print(" ");
+            }
+            for (int k=1; k<=i; k++){
+                System.out.print("*");
+            }
+            for (int s=1; s<i; s++){
+                System.out.print("*");
+            }
+            System.out.println();
+            
+        }
+       
+    }
+}
