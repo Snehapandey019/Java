b0214fdc20fd14603abd06b9d07fc83467a558e9
Sneha Pandey <sneha.cs231210@global.org.in>
2025-05-09 10:28:00 +0530

    hello

--- sneha/abc.java
+++
@@ -1,8 +0,0 @@
-package sneha;
-class abc{
-    public static void main(String[] args){
-        person obj = new person();
-        obj.SayHello();
-        System.out.println(obj.name);
-    }
-}
--- sneha/person.java
+++
@@ -1,7 +0,0 @@
-package sneha;
-class person{
-    void SayHello(){
-        System.out.println("hello");
-        int name = "Snehaaa";
-    }
-}
